3dbca3bdb3a78b2b629e23f7fe002bc4b57f3fde
John Paul Mariano <jppm8387@yahoo.com>
2023-08-10 20:30:02 -0400

    Icon

--- components/navigation/mainVerticalNavigation.tsx
+++ components/navigation/mainVerticalNavigation.tsx
@@ -6,6 +6,17 @@ import styles from "@/styles/components/logo/logo.module.scss";
 import { Collapse, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from '@mui/material';
 import { ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
 import CodeIcon from '@mui/icons-material/Code';
+import Icon from '@/components/icon';
+import javascriptSvg from '@/public/icons/javascript-svgrepo-com.svg';
+import htmlSvg from '@/public/icons/html-5-svgrepo-com.svg';
+import cssSvg from '@/public/icons/css3-svgrepo-com.svg';
+import reactSvg from '@/public/icons/react-svgrepo-com.svg';
+import nextjsSvg from '@/public/icons/nextjs-icon-svgrepo-com.svg';
+import drupalSvg from '@/public/icons/drupal-icon-svgrepo-com.svg';
+import vueSvg from '@/public/icons/vue-9-logo-svgrepo-com.svg';
+import SqlSvg from '@/public/icons/database-svgrepo-com.svg';
+import MongoDbSvg from '@/public/icons/mongodb-opened-svgrepo-com.svg';
+
 
 const MainVerticalNavigation: React.FC = () => {
 
@@ -17,16 +28,59 @@ const MainVerticalNavigation: React.FC = () => {
     >
       <ListItemButton>
         <ListItemIcon>
-          <CodeIcon />
+          <Icon svg={htmlSvg} alt={'HTML Icon'} size='md'/>
         </ListItemIcon>
         <ListItemText primary="HTML" />
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
-          <CodeIcon />
+          <Icon svg={cssSvg} alt={'CSS Icon'} size='md'/>
         </ListItemIcon>
-        <ListItemText primary="Drafts" />
+        <ListItemText primary="CSS" />
       </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={javascriptSvg} alt={'Javascript Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="Javascript" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={reactSvg} alt={'React Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="React" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={nextjsSvg} alt={'Next Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="NextJs" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={vueSvg} alt={'Vue Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="Vue" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={SqlSvg} alt={'SQL Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="SQL" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={MongoDbSvg} alt={'MongoDb Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="MongoDb" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemIcon>
+          <Icon svg={drupalSvg} alt={'Drupal Icon'} size='md'/>
+        </ListItemIcon>
+        <ListItemText primary="Drupal" />
+      </ListItemButton>
+      
     </List>
   );
 };
---
+++ public/icons/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '@/styles/components/icons/icons.module.scss';
+
+interface IconProps {
+  svg: string;
+  alt: string;
+  size?: 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'xxxl';
+}
+
+const Icon: React.FC<IconProps> = ({ svg, alt, size }) => {
+    switch(size) { 
+        case 'sm': { 
+            return <Image priority src={svg} alt={alt} className={styles.sm} />;
+           break; 
+        } 
+        case 'md': { 
+            return <Image priority src={svg} alt={alt} className={styles.md} />;
+           break; 
+        } 
+        case 'lg': { 
+            return <Image priority src={svg} alt={alt} className={styles.lg} />;
+           break; 
+        } 
+        case 'xl': { 
+            return <Image priority src={svg} alt={alt} className={styles.xl} />;
+           break; 
+        } 
+        case 'xxl': { 
+            return <Image priority src={svg} alt={alt} className={styles.xxl} />;
+           break; 
+        } 
+        case 'xxxl': { 
+            return <Image priority src={svg} alt={alt} className={styles.xxxl} />;
+           break; 
+        } 
+        default: { 
+            return <Image priority src={svg} alt={alt} className={styles.sm} />;
+           break; 
+        } 
+     } 
+  
+};
+
+export default Icon;
