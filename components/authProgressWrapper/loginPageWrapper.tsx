4502ef9672aedd10ea9f4c0472e3c28fecc3aebf
jmariano <jppm8387@yahoo.com>
2024-08-26 09:24:10 -0400

    Login and Logout

--- components/authProgressWrapper/loginPageWrapper.tsx
+++ components/authProgressWrapper/loginPageWrapper.tsx
@@ -2,7 +2,7 @@
 "use client";
 import React, { useEffect } from 'react';
 import { signOut, useSession } from 'next-auth/react';
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 import LinearProgress from '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
 import LinearLoading from '@/components/layouts/linearLoading';
---
+++ components/customSignOut/signInButton.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+
+import { useSession } from 'next-auth/react';
+import customSignOut from '.';
+import { Button, useTheme } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+import { redirect } from 'next/navigation';
+
+const SignInButton = () => {
+    
+
+    const handleSignIn = async () => {
+        redirect('/login');
+    };
+
+    const theme = useTheme();
+    let isLight: boolean = true;
+    if (theme.palette.mode === 'dark') {
+        isLight = false;
+    }
+    //'#1da8fe' 
+    return (
+        <Button onClick={handleSignIn} variant="contained" endIcon={<LoginIcon />} sx={{bgcolor: isLight ? '#003966'  : '#121212', 
+            "&:hover": { bgcolor: isLight ? '#1da8fe'  : '#ffffff', color:  isLight ? '#ffffff'  : '#000000',}, 
+            color:  '#ffffff'}}>
+            Sign In
+        </Button>
+    );
+};
+
+export default SignInButton;
