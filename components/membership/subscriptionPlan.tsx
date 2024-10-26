520789cfe935040d4692ca4e58517352e40bace8
jmariano <jppm8387@yahoo.com>
2024-10-25 20:43:17 -0400

    subscription

--- components/membership/subscriptionPlan.tsx
+++ components/membership/subscriptionPlan.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Paper, Typography, Button, useTheme } from '@mui/material';
 import { useMembershipContext } from './membershipProvider';
 import { useTabContext } from './TabProvider';
@@ -10,51 +10,28 @@ const SubscriptionPlan: React.FC = () => {
   const membershipContext = useMembershipContext();
   const tabContext = useTabContext();
   const theme = useTheme();
-  console.log("membershipContext", membershipContext.type);
+  const [content, setContent] = useState<React.ReactNode>(null);
+ //console.log("membershipContext", membershipContext.type);
+ useEffect(() => {
   if (membershipContext.type === null) {
-    return (
-      <Box component="div" className='flex justify-center'>
-        <Box className="p-4">
-          <Button variant="contained" className="flex justify-center flex-col" color="info" onClick={() => tabContext.setTabValue(1)}>
-            <Box component={"span"}> <Typography component="p" variant="body1" sx={{color: "#ffffff"}}>FREE PERSONAL MEMBERSHIP</Typography> </Box>
-            <Box component={"span"}> $0.00 USD per month </Box>
-            <Box component={"span"}> Signup </Box>
-          </Button>
+    setContent (
+
+        <Box component="div" className='flex justify-center'>
+          <Box className="p-4">
+            <Button variant="contained" className="flex justify-center flex-col" color="info" onClick={() => tabContext.setTabValue(1)}>
+              <Box component={"span"}> <Typography component="p" variant="body1" sx={{ color: "#ffffff" }}>FREE PERSONAL MEMBERSHIP</Typography> </Box>
+              <Box component={"span"}> $0.00 USD per month </Box>
+              <Box component={"span"}> Signup </Box>
+            </Button>
+          </Box>
         </Box>
-        <Box className="p-4">
-          <Button variant="contained" className="flex justify-center flex-col" color="info" onClick={() => tabContext.setTabValue(1)}>
-            <Box component={"span"}> <Typography component="p" variant="body1" sx={{color: "#ffffff"}}>MONTHLY PERSONAL MEMBERSHIP</Typography> </Box>
-            <Box component={"span"}> $25.00 USD per month </Box>
-            <Box component={"span"}> Signup </Box>
-          </Button>
-        </Box>
-        <Box className="p-4">
-          <Button variant="contained" className="flex justify-center flex-col"  color="info" onClick={() => tabContext.setTabValue(1)}>
-            <Box component={"span"}> <Typography component="p" variant="body1" sx={{color: "#ffffff"}}>ANNUAL PERSONAL MEMBERSHIP</Typography> </Box>
-            <Box component={"span"}> $240.00 USD per year </Box>
-            <Box component={"span"}> Signup </Box>
-          </Button>
-        </Box>
-        <Box className="p-4">
-          <Button variant="contained" className="flex justify-center flex-col"  color="info" onClick={() => tabContext.setTabValue(1)}>
-            <Box component={"span"}><Typography component="p" variant="body1" sx={{color: "#ffffff"}}> MONTHLY GROUP MEMBERSHIP</Typography> </Box>
-            <Box component={"span"}> $20.00 USD each member per month </Box>
-            <Box component={"span"}> Signup </Box>
-          </Button>
-        </Box>
-        <Box className="p-4">
-          <Button variant="contained" className="flex justify-center flex-col"  color="info" onClick={() => tabContext.setTabValue(1)}>
-            <Box component={"span"}> <Typography component="p" variant="body1" sx={{color: "#ffffff"}}>ANNUAL GROUP MEMBERSHIP </Typography></Box>
-            <Box component={"span"}> $180.00 USD each member per year </Box>
-            <Box component={"span"}> Signup </Box>
-          </Button>
-        </Box>
-      </Box>
+    
     );
   }
-
+}, [membershipContext.type]);
 
   return (
+    <>{content}
     <Box component="div" className='flex justify-center'>
         <Box className="p-4">
           <Button variant="contained" className="flex justify-center flex-col" color="info" onClick={() => tabContext.setTabValue(1)}>
@@ -85,6 +62,7 @@ const SubscriptionPlan: React.FC = () => {
           </Button>
         </Box>
       </Box>
+      </>
   );
 };
 
